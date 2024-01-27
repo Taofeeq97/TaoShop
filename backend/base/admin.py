a20b8a40a9fa012f7a9b480b48a425c91d2fe309
Taofeeq97 <otutaofeeqi@gmail.com>
2024-01-27 18:27:45 +0100

    added poduct db table

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'category', 'price', 'countInStock', 'created_at', 'updated_at')
+    list_filter = ('brand', 'category', 'created_at', 'updated_at')
+    search_fields = ('name', 'brand', 'category')
+    ordering = ('-created_at',)
+
+admin.site.register(Product, ProductAdmin)
+
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Product(models.Model):
+    user=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name=models.CharField(max_length=225, null=True, blank=True)
+    brand=models.CharField(max_length=225, null=True, blank=True)
+    # image=models.ImageField(upload_to='product/image')
+    category=models.CharField(max_length=225, null=True, blank=True)
+    description=models.TextField(max_length=225, null=True, blank=True)
+    rating=models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True)
+    price=models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
